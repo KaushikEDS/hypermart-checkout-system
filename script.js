97ab66e79c23384dc395934f304b66ed9af1a4dc
Kaushik Nippanikar <kaushiknippanikar@Kaushiks-MacBook-Pro.local>
2025-05-17 03:45:24 +0530

    Created counter class

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const counterContainer = document.querySelector('.counter-container');
+const counterContainer = document.querySelector('#counter-container');
 
 /**
  * This will display the max number of counters
@@ -6,10 +6,39 @@ const counterContainer = document.querySelector('.counter-container');
  */
 const maxCounters = 3;
 
+function getMaterialIcon(name) {
+    const materialIcon = document.createElement('span');
+    materialIcon.classList.add('material-symbols-outlined');
+    materialIcon.textContent = name;
+    return materialIcon;
+}
+
+function getCounterItem(count) {
+    const item = document.createElement('div');
+    item.classList.add('item');
+    const itemCount = document.createElement('div');
+    itemCount.classList.add('item-count');
+    itemCount.appendChild(getMaterialIcon('shopping_cart'));
+    itemCount.appendChild(document.createTextNode(`${count} items`));
+    item.appendChild(itemCount);
+    const removeItem = document.createElement('button');
+    // removeItem.onclick = event => event.target.remove();
+    item.appendChild(removeItem);
+    return item;
+}
+
 class CounterBuilder {
+    constructor(index) {
+        this.index = index;
+    }
+
     getCounter() {
-       const counter = document.createElement('div');
-       counter.classList.add('counter');
+        const counter = document.createElement('div');
+        counter.setAttribute('data-counter-index', this.index);
+        counter.classList.add('counter');
+        counter.appendChild(this.getCounterHeaderContainer());
+        counter.appendChild(this.getItemContainer());
+        return counter;
     }
 
     getCounterHeaderContainer() {
@@ -22,25 +51,37 @@ class CounterBuilder {
     getCounterHeader() {
         const counterHeader = document.createElement('div');
         counterHeader.classList.add('counter-header');
-        counterContainer.setAttribute();
+        counterHeader.textContent = `Counter ${this.index}`;
         return counterHeader;
     }
-    
-    
+
+    getCustomerCount() {
+        const customerCount = document.createElement('div');
+        customerCount.classList.add('customer-count');
+        customerCount.appendChild(getMaterialIcon('group'));
+        const customerCountText = document.createElement('span');
+        customerCountText.textContent = `0 Customers`;
+        customerCount.appendChild(customerCountText);
+        return customerCount;
+    }
+
+    getItemContainer() {
+        const itemContainer = document.createElement('div');
+        itemContainer.classList.add('item-container');
+        return itemContainer;
+    }
 }
 
 class Counter {
     constructor(index) {
         this.index = index;
-        this.counter = document.createElement('div');
-        this.counter.classList.add('counter');
+        const builder = new CounterBuilder(index);
+        this.counter = builder.getCounter();
+        this.itemContainer = this.counter.querySelector('.item-container');
         counterContainer.appendChild(this.counter);
     }
-
-    addItem(item) {
-        this.counter.appendChild(item);
-    }
-    
 }
 
-
+for (let i = 0; i < maxCounters; i++) {
+    new Counter(i + 1);
+}
