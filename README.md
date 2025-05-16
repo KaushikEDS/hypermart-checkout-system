ccfcc7f4f57904dc64b03e3ed58b599572e26d9b
Kaushik Nippanikar <kaushik@Kaushiks-Mac-mini.local>
2025-05-17 02:58:46 +0530

    Second commit

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 * Hypermark Checkout System
 
 ## About
-* 
+I noticed a point where it said I have to make this using HTML,CSS and JS. Having years of experience instinctively tells us to create a React or Angular app.
+
+To mimic an architecture like React and Angular I did what most developers do, that is use lots of javascript. To keep it scalable I have initialized a variable "maxCounters", we can easily add any functionality to this project to make it dynamic.
+
+I have created class "Counter" to handle the basic operations which will happen after the input changes.
+
+I have created class "CounterBuilder" to handle one time operations needed to builder Counter element. I could've kept all the logic inside Counter class itself, but it just waste memory since after building those functions wont be called anyways.
+
+
+
+
--- script.js
+++ script.js
@@ -6,10 +6,41 @@ const counterContainer = document.querySelector('.counter-container');
  */
 const maxCounters = 3;
 
-function createCounter(index) {
-    const counter = document.createElement('div');
-    counter.classList.add('counter');
-    counterContainer.appendChild(counter);
+class CounterBuilder {
+    getCounter() {
+       const counter = document.createElement('div');
+       counter.classList.add('counter');
+    }
+
+    getCounterHeaderContainer() {
+        const counterHeader = document.createElement('div');
+        counterHeader.classList.add('counter-header-container');
+        counterHeader.appendChild(this.getCounterHeader());
+        return counterHeader;
+    }
+
+    getCounterHeader() {
+        const counterHeader = document.createElement('div');
+        counterHeader.classList.add('counter-header');
+        counterContainer.setAttribute();
+        return counterHeader;
+    }
+    
+    
+}
+
+class Counter {
+    constructor(index) {
+        this.index = index;
+        this.counter = document.createElement('div');
+        this.counter.classList.add('counter');
+        counterContainer.appendChild(this.counter);
+    }
+
+    addItem(item) {
+        this.counter.appendChild(item);
+    }
+    
 }
 
 
