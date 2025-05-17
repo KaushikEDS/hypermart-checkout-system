e2cb65bb1acb84c38b763598b7c3b93177a45b04
Kaushik Nippanikar <kaushiknippanikar@Kaushiks-MacBook-Pro.local>
2025-05-17 13:18:12 +0530

    Functionality done

--- README.md
+++ README.md
@@ -4,11 +4,14 @@
 ## About
 I noticed a point where it said I have to make this using HTML,CSS and JS. Having years of experience instinctively tells us to create a React or Angular app.
 
-To mimic an architecture like React and Angular I did what most developers do, that is use lots of javascript. To keep it scalable I have initialized a variable "maxCounters", we can easily add any functionality to this project to make it dynamic.
+To mimic an architecture like React and Angular I did what most developers do, that is use lots of javascript. To keep it scalable I have initialized a variable "maxCounters", we can easily add any functionality to this project to make it dynamic. I have kept my logic independant of maxCounters
 
-I have created class "Counter" to handle the basic operations which will happen after the input changes.
+I have created class "Counter" to handle the basic operations which will happen after the input changes. I have put functions inside Counter class which can will be commonly used.
 
 I have created class "CounterBuilder" to handle one time operations needed to builder Counter element. I could've kept all the logic inside Counter class itself, but it just waste memory since after building those functions wont be called anyways.
+
+The tricky part is keeping the leftmost counter as minCounter, It took me a while to figure out. My goal is to not keep multiple loops to find it.
+
 
 
 
--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-const counterContainer = document.querySelector('#counter-container');
+class Counter {
+    constructor(index) {
+        this.index = index;
+        const builder = new CounterBuilder(index);
+        this.counter = builder.getCounter();
+        this.itemContainer = this.counter.querySelector('.item-container');
+        this.customerCount = this.counter.querySelector(`#customer-count-${this.index}`);
+        counterContainer.appendChild(this.counter);
+    }
 
-/**
- * This will display the max number of counters
- * We can changes this as per requirement
- */
-const maxCounters = 3;
+    get totalItemCount() {
+        return parseInt(this.counter.getAttribute('data-total-items'));
+    }
 
-function getMaterialIcon(name) {
-    const materialIcon = document.createElement('span');
-    materialIcon.classList.add('material-symbols-outlined');
-    materialIcon.textContent = name;
-    return materialIcon;
-}
+    update() {
+        this.calculateTotalItems();
+        this.calculateCustomerCount();
+    }
 
-function getCounterItem(count) {
-    const item = document.createElement('div');
-    item.classList.add('item');
-    const itemCount = document.createElement('div');
-    itemCount.classList.add('item-count');
-    itemCount.appendChild(getMaterialIcon('shopping_cart'));
-    itemCount.appendChild(document.createTextNode(`${count} items`));
-    item.appendChild(itemCount);
-    const removeItem = document.createElement('button');
-    // removeItem.onclick = event => event.target.remove();
-    item.appendChild(removeItem);
-    return item;
+    calculateTotalItems() {
+        let totalItems = 0;
+        this.itemContainer.childNodes.forEach(x => (totalItems += parseInt(x.getAttribute('data-item-count'))));
+        this.counter.setAttribute('data-total-items', totalItems);
+        return totalItems;
+    }
+
+    calculateCustomerCount() {
+        this.customerCount.textContent = `${this.totalItemCount} Customers`;
+    }
 }
 
 class CounterBuilder {
@@ -35,6 +37,7 @@ class CounterBuilder {
     getCounter() {
         const counter = document.createElement('div');
         counter.setAttribute('data-counter-index', this.index);
+        counter.setAttribute('data-total-items', 0);
         counter.classList.add('counter');
         counter.appendChild(this.getCounterHeaderContainer());
         counter.appendChild(this.getItemContainer());
@@ -45,6 +48,7 @@ class CounterBuilder {
         const counterHeader = document.createElement('div');
         counterHeader.classList.add('counter-header-container');
         counterHeader.appendChild(this.getCounterHeader());
+        counterHeader.appendChild(this.getCustomerCount());
         return counterHeader;
     }
 
@@ -60,6 +64,7 @@ class CounterBuilder {
         customerCount.classList.add('customer-count');
         customerCount.appendChild(getMaterialIcon('group'));
         const customerCountText = document.createElement('span');
+        customerCountText.id = `customer-count-${this.index}`;
         customerCountText.textContent = `0 Customers`;
         customerCount.appendChild(customerCountText);
         return customerCount;
@@ -72,16 +77,76 @@ class CounterBuilder {
     }
 }
 
-class Counter {
-    constructor(index) {
-        this.index = index;
-        const builder = new CounterBuilder(index);
-        this.counter = builder.getCounter();
-        this.itemContainer = this.counter.querySelector('.item-container');
-        counterContainer.appendChild(this.counter);
-    }
-}
+const counterContainer = document.querySelector('#counter-container');
+
+/**
+ * This will display the max number of counters
+ * We can changes this as per requirement
+ */
+const maxCounters = 3;
+
+const allCounters = [];
 
 for (let i = 0; i < maxCounters; i++) {
-    new Counter(i + 1);
+    allCounters.push(new Counter(i + 1));
 }
+
+let minCounter = allCounters[0];
+
+const inputField = document.querySelector('#input-field');
+
+const checkoutButton = document.querySelector('#checkout-button');
+
+checkoutButton.onclick = event => {
+    const inputValue = inputField.value;
+    if (inputValue > 0) {
+        minCounter.itemContainer.appendChild(getCounterItem(inputValue, minCounter));
+        minCounter.update();
+        calculateMinCounter();
+    }
+    inputField.value = '';
+};
+function getMaterialIcon(name) {
+    const materialIcon = document.createElement('span');
+    materialIcon.classList.add('material-symbols-outlined');
+    materialIcon.textContent = name;
+    return materialIcon;
+}
+
+function getCounterItem(count, counter) {
+    const item = document.createElement('div');
+    item.setAttribute('data-item-count', count);
+    item.classList.add('item');
+    const itemCount = document.createElement('div');
+    itemCount.classList.add('item-count');
+    itemCount.appendChild(getMaterialIcon('shopping_cart'));
+    itemCount.appendChild(document.createTextNode(`${count} items`));
+    item.appendChild(itemCount);
+    const removeItem = document.createElement('button');
+    removeItem.classList.add('remove-item');
+    removeItem.textContent = '-';
+    item.appendChild(removeItem);
+    removeItem.onclick = event => {
+        item.remove(); 
+        counter.update();
+        calculateMinCounter();
+    };
+    return item;
+}
+
+function calculateMinCounter() {
+    allCounters.forEach(counter => {
+        const currentItemCount = counter.totalItemCount;
+        const minItemCount = minCounter.totalItemCount;
+        if (currentItemCount < minItemCount) {
+            minCounter = counter;
+        }
+        /**
+         * This is to keep the leftmost counter as minCounter
+         */
+        else if (currentItemCount === minItemCount && counter.index < minCounter.index) {
+            minCounter = counter;
+        }
+    });
+}
+
